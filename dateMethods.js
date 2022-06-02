bb08b98bbee806050455bf11d3bdc6c37382bc68
charles okunzo <okunzopeter01@gmail.com>
2022-06-02 12:00:58 +0300

    add project files

---
+++ dateMethods.js
@@ -0,0 +1,45 @@
+// var date= new Date();
+// console.log(date)
+// console.log(date.toUTCString());
+// console.log(date.toString())
+// console.log(date.toDateString())
+// var date1= new Date(2022,1,1);
+// console.log(date1.toUTCString());
+// console.log(date1.toDateString());
+
+//ISO- syntax yyyy-mm-dd
+// var myDay= new Date("1998-12-16");
+// console.log(myDay.toString());
+
+//ISO- syntax (yyyy-mm-ddThh:mm:ss)
+
+// var myday= new Date("1998-12-16T06:30:00Z")
+
+// console.log(myday.toString())
+
+//short dates syntax- mm/dd/yyyy
+// var newDay=new Date("12/06/2001");
+// console.log(newDay.toString())
+
+//Long date
+
+// var longDate= new Date();
+// console.log(longDate);
+// console.log(longDate.getDate())
+// console.log(longDate.getDay())
+// console.log(longDate.getTime())
+// console.log(longDate.getFullYear())
+
+// var myDays= ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Sato"];
+
+//   console.log(myDays[longDate.getDay()])
+
+//Setting dates
+
+// var myDate=new Date();
+// console.log(new Date(myDate.setFullYear(2022,0,31)))
+
+// var me= Date.parse("jan 30 2020");
+// console.log(new Date(me))
+// var okay= 1237;
+// okay.valueOf();
